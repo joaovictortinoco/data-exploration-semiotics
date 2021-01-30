e15c9963509ed2c710588384160e48d9ee5c582f
João Victor Tinoco <joaovictortinoco@gmail.com>
2021-01-30 17:07:28 -0300

    Genetic programming v1

---
+++ src/models/genetic_programming.py
@@ -0,0 +1,41 @@
+import operator
+
+from deap.gp import PrimitiveSetTyped, PrimitiveTree, genFull, graph
+from deap import creator, base, tools
+from pygraphviz import AGraph
+
+class GeneticProgramming(object):
+    def __init__(self):
+        super.__init__()
+
+    def if_then_else(self, input, output1, output2):
+        return output1 if input else output2
+
+    def setPrimitive(self):
+        pset = PrimitiveSetTyped("main", [bool, float], float)
+        pset.addPrimitive(operator.xor, [bool, bool], bool)
+        pset.addPrimitive(operator.mul, [float, float], float)
+        pset.addPrimitive(self.if_then_else, [bool, float, float], float)
+        pset.addTerminal(3.0, float)
+        pset.addTerminal(1, bool)
+
+        return pset
+
+    def renameArgs(self, pset, x, y):
+        pset.renameArguments(ARG0=x)
+        pset.renameArguments(ARG1=y)
+
+        return pset
+
+    def generateTree(self, pset, min, max):
+        return genFull(pset, min_=min, max_=max)
+
+    def createIndividuals(self, weights):
+        creator.create("Fitness", base.Fitness, weights=(-1, 1))
+        creator.create("Individuals", PrimitiveTree, fitness=creator.Fitness, pset=pset)
+
+        toolbox = base.Toolbox()
+        toolbox.register("expr", genFull, pset=pset, min_=1, max_=3)
+        toolbox.register("individual", tools.initIterate, creator.Individuals, toolbox.expr)
+
+        return toolbox.individual()
